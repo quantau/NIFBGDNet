1fd48a61a21b9b3082e3e5042879ff84e6de87f8
HarshitSingh <geniousharshit@gmail.com>
2023-09-11 22:46:54 +0530

    allowed recursive search for images

--- Generate_Patches_Gray.py
+++ Generate_Patches_Gray.py
@@ -1,24 +1,41 @@
 # import the libraries
 import os
+import argparse
 import numpy as np
 import cv2
 from conf import myConfig_Gray as config
 from pathlib import Path
+from tqdm import tqdm
 
 # define scales
 scales=[1,0.9,0.8,0.7]
 count=0
 
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--src_dir', dest='src_dir',
+                    default='/home/anirban_2021cs13/Proj_RK/Dataset/Dataset', help='dir of data')
+args = parser.parse_args()
+
 # create the imageArrays
 p=Path(config.genDataPath)
-listPaths=list(p.glob('./*.png'))
+# listPaths=list(p.glob('./*.tif'))
+listPaths = []
+
+for root, _, files in tqdm(os.walk(args.src_dir)):
+    for file in files:
+        if file.endswith(".tif"):
+            # Construct the full file path
+            file_path = os.path.join(root, file)
+            listPaths.append(file_path)
+
 imgArray=[]
 for path in listPaths:
     imgArray.append(cv2.imread(str(path),0))
 print('lenImages',len(imgArray))
 
 #calculate the number of patches
-for i in range(len(imgArray)):
+print("Calculating the number of patches")
+for i in tqdm(range(len(imgArray))):
     img = imgArray[i] 
     for s in range(len(scales)):
         newsize=(int(img.shape[0]*scales[s]),int(img.shape[1]*scales[s]))
@@ -42,8 +59,9 @@ print('total patches=%d, batch_size=%d, total_batches=%d' %
 inputs=np.zeros((int(numPatches), int(config.pat_size), int(config.pat_size),1),dtype=np.uint8)
 
 #generate patches
+print("Genrating patches")
 count=0
-for i in range(len(imgArray)):
+for i in tqdm(range(len(imgArray))):
     img=imgArray[i]
     for s in range(len(scales)):
         newsize=(int(img.shape[0]*scales[s]),int(img.shape[1]*scales[s]))
