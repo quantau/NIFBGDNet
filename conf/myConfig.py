d986ac8784192ef470027159c4210c2daf9b2aac
HarshitSingh <geniousharshit@gmail.com>
2023-09-11 19:40:40 +0530

    resolved minor bugs

--- conf/myConfig.py
+++ conf/myConfig.py
@@ -2,24 +2,24 @@
 import os
 
 #default number of epochs
-epochs=40
+epochs = 40
 
 #default batch size for training
-batch_size=32
+batch_size = 32
 
 #noise-level for training
-sigma=25.0  #change it according to noise level in your dataset
+sigma = 25.0  # change it according to noise level in your dataset
 
 #path to generate the data
-genDataPath='./BSD400/'
+genDataPath = './BSD400/'
 
 #path to save the genPatches
-save_dir='./trainingPatch/'
+save_dir = './trainingPatch/'
 
 #path to training data
-data='./trainingPatch/clean_pats_rgb.npy'
+data = './trainingPatch/img_clean_pats.npy'
 
 #variables required to generate patch
-pat_size=30
-stride=10
-step=0
+pat_size = 30
+stride = 10
+step = 0
